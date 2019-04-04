667cbfb3fd5baa96be03cea04730beb4f1806209
Gabriel Adorf <gabriel.adorf@gmail.com>
2019-04-04 23:02:09 +0200

    add new global styles

---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+export const color = {
+  grey900: '#1B1B1B',
+  grey800: '#2B2B2B',
+  grey700: '#434343',
+  grey600: '#6E6E6E',
+  grey500: '#999999',
+  grey400: '#C4C4C4',
+  grey300: '#E0E0E0',
+  grey200: '#EDEDED',
+  grey100: '#F9F9F9',
+}
+
+export const fontSize = {
+  f11: '58px',
+  f10: '48px',
+  f9: '42px',
+  f8: '36px',
+  f7: '30px',
+  f6: '24px',
+  f5: '20px',
+  f4: '18px',
+  f3: '16px',
+  f2: '14px',
+  f1: '12px',
+}
