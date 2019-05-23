6cdf001258af909dfd02f4e1320ea343b8425c01
Gabriel Adorf <gabriel.adorf@gmail.com>
2019-05-23 09:31:01 +0200

    hide breakpoints provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   render() {
     return (
       <GlobalWrapper>
-        <Breakpoints />
+        {/* <Breakpoints /> */}
         <Hero />
         <Section id={'about'}>
           <About />
