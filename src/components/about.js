20f66148f2aa80a10469096d4c8da9c1f6de14b0
Gabriel Adorf <gabriel.adorf@gmail.com>
2020-01-12 22:04:36 +0100

    remove old code

--- src/components/about.js
+++ src/components/about.js
@@ -7,18 +7,12 @@ import { color, fontSize } from 'styles/theme'
 
 import TwoColumns from 'components/twoColumns'
 import SectionHeading from 'components/sectionHeading'
-// import WhenInView from 'components/wheninView'
-
-// const Reveal = styled.div`
-//   opacity: ${props => (props.visible ? '1' : '0')};
-//   transform: translateY(${props => (props.visible ? '0px' : '30px')});
-//   transition: all 1s;
-// `
 
 const Big = styled.span`
   font-size: ${fontSize.f6};
   color: ${color.grey900};
   font-weight: 700;
+  letter-spacing: -0.4px;
   ${media.sm`
     font-size: ${fontSize.f5};
   `}
