5841d03248406f0bc38d457a4424e750b702d110
Gabriel Adorf <gabriel.adorf@gmail.com>
2020-04-30 17:44:23 +0200

    refactor breakpoints component

--- src/utils/breakpoints.js
+++ src/utils/breakpoints.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const Div = styled.div`
@@ -10,11 +10,11 @@ const Div = styled.div`
 `
 
 const BreakpointWrapper = styled.div`
-  display: ${props => (props.breakpointsAreVisible ? 'block' : 'none')};
+  display: ${(props) => (props.breakpointsAreVisible ? 'block' : 'none')};
 `
 
 const ScreenWrapper = styled.div`
-  display: ${props => (props.screensAreVisible ? 'block' : 'none')};
+  display: ${(props) => (props.screensAreVisible ? 'block' : 'none')};
 `
 
 const Breakpoint = styled.div`
@@ -28,9 +28,9 @@ const Breakpoint = styled.div`
   border-right: 1px solid red;
   z-index: 100000;
   height: 500vh;
-  width: ${props => props.width};
+  width: ${(props) => props.width};
   &::before {
-    content: '${props => props.name}';
+    content: '${(props) => props.name}';
   }
 `
 
@@ -45,10 +45,10 @@ const Screen = styled.div`
   border-right: 1px solid lime;
   border-bottom: 1px solid lime;
   z-index: 100000;
-  height: ${props => props.height};
-  width: ${props => props.width};
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
   &::before {
-    content: '${props => props.name}';
+    content: '${(props) => props.name}';
   }
 `
 
@@ -90,71 +90,57 @@ const Button2 = styled.input`
   }
 `
 
-class Breakpoints extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      breakpointsAreVisible: false,
-      screensAreVisible: false,
-      breakpointsButtonLabel: 'SHOW BREAKPOINTS',
-      screensButtonLabel: 'SHOW SCREEN SIZES'
-    }
-    this.handleBreakpointClick = this.handleBreakpointClick.bind(this)
-    this.handleScreenClick = this.handleScreenClick.bind(this)
-  }
-  
-  handleBreakpointClick() {
-    if(this.state.breakpointsAreVisible === true) {
-      this.setState({
-        breakpointsAreVisible: !this.state.breakpointsAreVisible,
-        breakpointsButtonLabel: 'SHOW BREAKPOINTS'
-      })
-    } else {
-      this.setState({
-        breakpointsAreVisible: !this.state.breakpointsAreVisible,
-        breakpointsButtonLabel: 'HIDE BREAKPOINTS'
-      })
-    }
-  }
+const Breakpoints = () => {
+  const [breakpointsAreVisible, setBreakpointsAreVisible] = useState('')
+  const [screensAreVisible, setScreensAreVisible] = useState('')
+  const [breakpointsButtonLabel, setBreakpointsButtonLabel] = useState(
+    'SHOW BREAKPOINTS'
+  )
+  const [screensButtonLabel, setScreensButtonLabel] = useState(
+    'SHOW SCREEN SIZES'
+  )
 
-  handleScreenClick() {
-    console.log(this.state)
-    if(this.state.screensAreVisible === true) {
-      this.setState({
-        screensAreVisible: !this.state.screensAreVisible,
-        screensButtonLabel: 'SHOW SCREEN SIZES'
-      })
-    } else {
-      this.setState({
-        screensAreVisible: !this.state.screensAreVisible,
-        screensButtonLabel: 'HIDE SCREEN SIZES'
-      })
-    }
-    console.log(this.state.screensAreVisible);
-    console.log(this.state.breakpointsAreVisible);
-  }
-
-  render() {
-    return (
-      <Div>
-        <Button value={this.state.breakpointsButtonLabel} type='button' onClick={this.handleBreakpointClick} />
-        <Button2 value={this.state.screensButtonLabel} type='button' onClick={this.handleScreenClick} />
-        <BreakpointWrapper breakpointsAreVisible={this.state.breakpointsAreVisible}>
-          <Breakpoint width='350px' name='XS' />
-          <Breakpoint width='600px' name='SM' />
-          <Breakpoint width='900px' name='MD' />
-          <Breakpoint width='1300px' name='LG' />
-          <Breakpoint width='1700px' name='XL' />
-        </BreakpointWrapper>
-        <ScreenWrapper screensAreVisible={this.state.screensAreVisible}>
-          <Screen width='375px' height='667px' name='iPhone'/>
-          <Screen width='1280px' height='699px' name='MBP'/>
-          <Screen width='1680px' height='949px' name='MBP scaled'/>
-          <Screen width='2560px' height='1339px' name='Dell'/>
-        </ScreenWrapper>
-      </Div>
+  const handleBreakpointClick = () => {
+    setBreakpointsButtonLabel(
+      breakpointsAreVisible ? 'SHOW BREAKPOINTS' : 'HIDE BREAKPOINTS'
     )
+    setBreakpointsAreVisible(!breakpointsAreVisible)
   }
+
+  const handleScreenClick = () => {
+    setScreensButtonLabel(
+      screensAreVisible ? 'SHOW SCREEN SIZES' : 'HIDE SCREEN SIZES'
+    )
+    setScreensAreVisible(!screensAreVisible)
+  }
+
+  return (
+    <Div>
+      <Button
+        value={breakpointsButtonLabel}
+        type="button"
+        onClick={handleBreakpointClick}
+      />
+      <Button2
+        value={screensButtonLabel}
+        type="button"
+        onClick={handleScreenClick}
+      />
+      <BreakpointWrapper breakpointsAreVisible={breakpointsAreVisible}>
+        <Breakpoint width="350px" name="XS" />
+        <Breakpoint width="600px" name="SM" />
+        <Breakpoint width="900px" name="MD" />
+        <Breakpoint width="1300px" name="LG" />
+        <Breakpoint width="1700px" name="XL" />
+      </BreakpointWrapper>
+      <ScreenWrapper screensAreVisible={screensAreVisible}>
+        <Screen width="375px" height="667px" name="iPhone" />
+        <Screen width="1280px" height="699px" name="MBP" />
+        <Screen width="1680px" height="949px" name="MBP scaled" />
+        <Screen width="2560px" height="1339px" name="Dell" />
+      </ScreenWrapper>
+    </Div>
+  )
 }
 
 export default Breakpoints
